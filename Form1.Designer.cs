5f3663cc2b579ecd912e5857bea3b50c2046ad67
kevinkdeveloper <kevin.koo@live.com>
2025-04-28 09:55:52 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Astronomical_Processing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NeutrinoDataListBox = new System.Windows.Forms.ListBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtEditIndex = new System.Windows.Forms.TextBox();
+            this.txtEditValue = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NeutrinoDataListBox
+            // 
+            this.NeutrinoDataListBox.FormattingEnabled = true;
+            this.NeutrinoDataListBox.ItemHeight = 16;
+            this.NeutrinoDataListBox.Location = new System.Drawing.Point(34, 190);
+            this.NeutrinoDataListBox.Name = "NeutrinoDataListBox";
+            this.NeutrinoDataListBox.Size = new System.Drawing.Size(278, 404);
+            this.NeutrinoDataListBox.TabIndex = 0;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(103, 62);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(100, 22);
+            this.txtSearch.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(224, 62);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(117, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Binary Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtEditIndex
+            // 
+            this.txtEditIndex.Location = new System.Drawing.Point(103, 107);
+            this.txtEditIndex.Name = "txtEditIndex";
+            this.txtEditIndex.Size = new System.Drawing.Size(100, 22);
+            this.txtEditIndex.TabIndex = 3;
+            // 
+            // txtEditValue
+            // 
+            this.txtEditValue.Location = new System.Drawing.Point(103, 145);
+            this.txtEditValue.Name = "txtEditValue";
+            this.txtEditValue.Size = new System.Drawing.Size(100, 22);
+            this.txtEditValue.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Search";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Index";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Value";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(224, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Bubble Sort";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(224, 122);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Edit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 635);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEditValue);
+            this.Controls.Add(this.txtEditIndex);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.NeutrinoDataListBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox NeutrinoDataListBox;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtEditIndex;
+        private System.Windows.Forms.TextBox txtEditValue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Astronomical_Processing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            DataGeneration();
+
+            DisplayNeutrinoData();
+        }
+
+        static int max = 24;
+        int[] neutrinoData = new int[max];
+
+        private void DataGeneration()
+        {
+            Random random = new Random();
+            for (int i = 0; i < max; i++)
+            {
+                neutrinoData[i] = random.Next(10, 91); // Rnage from 10 to 90
+            }
+        }
+
+        private void DisplayNeutrinoData()
+        {
+            NeutrinoDataListBox.Items.Clear();
+            for (int i = 0; i < max; i++)
+            {
+                NeutrinoDataListBox.Items.Add(neutrinoData[i]);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int temp = 0;
+            for (int outer = 0; outer < max; outer++)
+            {
+                for (int inner = 0; inner < max - 1; inner++)
+                {
+                    if (neutrinoData[inner] > neutrinoData[inner + 1])
+                    {
+                        // Swap the elements
+                        temp = neutrinoData[inner];
+                        neutrinoData[inner] = neutrinoData[inner + 1];
+                        neutrinoData[inner + 1] = temp;
+                    }
+                    ShowArray();
+                }
+            }
+        }
+        private void ShowArray()
+        {
+            NeutrinoDataListBox.Items.Clear();
+            for (int i = 0; i < max; i++)
+            {
+                NeutrinoDataListBox.Items.Add(neutrinoData[i]);
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            button1_Click(sender, e); // Sort the array first
+            MessageBox.Show("Array sorted. Now searching for the value...");
+
+            int mid;
+            int low = 0;
+            int high = max;
+            int target;
+
+            if (!(Int32.TryParse(txtSearch.Text, out target)))
+            {
+                MessageBox.Show("Please enter a valid number.");
+                return;
+            }
+
+            // Binary Search
+            while (low <= high)
+            {
+                mid = (low + high) / 2;
+                if (neutrinoData[mid] == target)
+                {
+                    MessageBox.Show($"Found {target} at index {mid}.");
+                    return;
+                }
+                else if (neutrinoData[mid] < target)
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Edit values at specified index
+            int index;
+            if (!Int32.TryParse(txtEditIndex.Text, out index))
+            {
+                MessageBox.Show("Please enter a valid index.");
+                return;
+            }
+            else
+            {
+                neutrinoData[index] = Int32.Parse(txtEditValue.Text);
+                // Show message of edited value
+                MessageBox.Show($"Index:{index}, changed to:{txtEditValue.Text}.");
+                ShowArray();
+            } 
+        }
+    }
+}
